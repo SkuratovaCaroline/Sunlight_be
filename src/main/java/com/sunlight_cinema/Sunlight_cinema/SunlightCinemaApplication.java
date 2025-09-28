691f0d3b0d35b4139444e3d2ce37d657e38c6044
Polina Valkovskaya <sunfllorist@gmail.com>
2025-09-28 23:35:53 +0300

    Initial backend commit

---
+++ src/main/java/com/sunlight_cinema/Sunlight_cinema/SunlightCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.sunlight_cinema.Sunlight_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunlightCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunlightCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunlight_cinema/Sunlight_cinema/SunlightCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunlight_cinema.Sunlight_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlightCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
