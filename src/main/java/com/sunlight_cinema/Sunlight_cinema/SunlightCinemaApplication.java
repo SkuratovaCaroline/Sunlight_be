fa5d4d366a3ffbb5e8bcc20739ad129f06f2f748
polya-polya <sunfllorist@gmail.com>
2025-09-29 00:03:55 +0300

    Update SunlightCinemaApplication.java

--- src/main/java/com/sunlight_cinema/Sunlight_cinema/SunlightCinemaApplication.java
+++ src/main/java/com/sunlight_cinema/Sunlight_cinema/SunlightCinemaApplication.java
@@ -11,3 +11,14 @@ public class SunlightCinemaApplication {
 	}
 
 }
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "Sunlight Cinema API is running!";
+    }
+}
