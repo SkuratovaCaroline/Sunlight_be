3655d2fa7c5773e813535ed5400683c6f5740a66
polya-polya <sunfllorist@gmail.com>
2025-10-14 22:14:17 +0300

    Update SunlightCinemaApplication.java

--- src/main/java/com/sunlight_cinema/Sunlight_cinema/SunlightCinemaApplication.java
+++ src/main/java/com/sunlight_cinema/Sunlight_cinema/SunlightCinemaApplication.java
@@ -2,12 +2,18 @@ package com.sunlight_cinema.Sunlight_cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SunlightCinemaApplication {
+public class SunlightCinemaApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SunlightCinemaApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SunlightCinemaApplication.class, args);
     }
-
 }
