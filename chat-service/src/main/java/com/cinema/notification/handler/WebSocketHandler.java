1f3f6ef8ab297b838d92830f55cbdd063967bdab
SkuratovaCaroline <skuratova.c@gmail.com>
2025-11-08 00:35:52 +0300

    Create WebSocketHandler.java

---
+++ chat-service/src/main/java/com/cinema/notification/handler/WebSocketHandler.java
@@ -0,0 +1,24 @@
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+
+    public void broadcast(String message) {
+        sessions.forEach(session -> {
+            try {
+                session.sendMessage(new TextMessage(message));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
