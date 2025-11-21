fb9297589b617c405518a4365d3e7b12d039eb0d
SkuratovaCaroline <skuratova.c@gmail.com>
2025-11-21 23:11:19 +0300

    Update Service.java

--- functional/src/main/java/com/sunlight_cinema/functional/Service.java
+++ functional/src/main/java/com/sunlight_cinema/functional/Service.java
@@ -7,19 +7,27 @@ public class CinemaService {
     private final RatingRepository ratingRepository;
     private final TicketRepository ticketRepository;
     private final NotificationRepository notificationRepository;
+    private final SeatRepository seatRepository;
+    private final SessionRepository sessionRepository;
 
     public CinemaService(UserRepository userRepository,
                          FilmRepository filmRepository,
                          ReviewRepository reviewRepository,
                          RatingRepository ratingRepository,
                          TicketRepository ticketRepository,
-                         NotificationRepository notificationRepository) {
+                         NotificationRepository notificationRepository,
+                         SeatRepository seatRepository,
+                         TicketRepository ticketRepository,
+                         SessionRepository sessionRepositor) {
         this.userRepository = userRepository;
         this.filmRepository = filmRepository;
         this.reviewRepository = reviewRepository;
         this.ratingRepository = ratingRepository;
         this.ticketRepository = ticketRepository;
         this.notificationRepository = notificationRepository;
+        this.seatRepository = seatRepository;
+        this.ticketRepository = ticketRepository;
+        this.sessionRepository = sessionRepository;
     }
 
     // --- USERS ---
@@ -38,8 +46,30 @@ public class CinemaService {
     public String getAvatar(Long userId) {
         return userRepository.findById(userId).map(User::getAvatarPath).orElse(null);
     }
+    
+    // --- ЗАНЯТЫЕ И СВОБОДНЫЕ МЕСТА ---
+    public Map<String, List<Seat>> getSeatsStatus(Long sessionId) {
+        Session session = sessionRepository.findById(sessionId).orElseThrow();
+        Long hallId = session.getHall().getHallId();
 
-    // --- FILMS ---
+        // все места в зале
+        List<Seat> allSeats = seatRepository.findByHallId(hallId);
+
+        // занятые места (по билетам)
+        List<Seat> occupiedSeats = ticketRepository.findOccupiedSeatsBySession(sessionId);
+
+        // свободные
+        List<Seat> freeSeats = allSeats.stream()
+                .filter(seat -> !occupiedSeats.contains(seat))
+                .toList();
+
+        Map<String, List<Seat>> result = new HashMap<>();
+        result.put("occupied", occupiedSeats);
+        result.put("free", freeSeats);
+        return result;
+    }
+
+    // --- Фильмы ---
     public Film addPoster(Long filmId, String posterPath) {
         Film film = filmRepository.findById(filmId).orElseThrow();
         film.setPosterPath(posterPath);
@@ -54,7 +84,36 @@ public class CinemaService {
         return ratingRepository.findAverageRatingByFilmId(filmId);
     }
 
-    // --- REVIEWS ---
+     // --- ПОКУПКА БИЛЕТА ---
+    public Ticket purchaseTicket(Long userId, Long sessionId, Long seatId, Double price) {
+        User user = userRepository.findById(userId).orElseThrow();
+        Session session = sessionRepository.findById(sessionId).orElseThrow();
+        Seat seat = seatRepository.findById(seatId).orElseThrow();
+
+        // Проверка: место уже занято?
+        if (ticketRepository.existsBySessionAndSeat(session, seat)) {
+            throw new RuntimeException("Seat already booked");
+        }
+
+        Ticket ticket = new Ticket();
+        ticket.setUser(user);
+        ticket.setSession(session);
+        ticket.setSeat(seat);
+        ticket.setPrice(price);
+        ticket.setStatus("SOLD");
+        ticket.setSoldAt(LocalDateTime.now());
+
+        return ticketRepository.save(ticket);
+    }
+
+    // --- РАСПИСАНИЕ ФИЛЬМА ---
+    public List<Session> getFilmSchedule(Long filmId) {
+        Film film = filmRepository.findById(filmId).orElseThrow();
+        return sessionRepository.findByFilmOrderByStartTimeAsc(film);
+    }
+
+
+    // --- Отзывы ---
     public Review createReview(Long userId, Long filmId, String text) {
         Review review = new Review();
         review.setUserId(userId);
@@ -67,7 +126,7 @@ public class CinemaService {
         return reviewRepository.findByFilmIdAndIsApprovedTrueOrderByReviewDateDesc(filmId);
     }
 
-    // --- RATINGS ---
+    // --- Рейтинг ---
     public UserRating createOrUpdateRating(Long userId, Long filmId, int rating) {
         return ratingRepository.saveOrUpdate(userId, filmId, rating);
     }
